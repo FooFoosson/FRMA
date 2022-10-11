9f6bd2721b0475ed1c3e73b3166317fecfe44ccc
Aleksandar Radovic <aleksandar.radovic@syrmia.com>
2022-10-11 20:38:00 +0200

    updated test with the new api and added more use case coverage

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,18 @@
 #include "perf_test_api.h"
 #include <thread>
+#include <iostream>
+
+class Test {
+public:
+	int a;
+	void foo(int c)
+	{
+		a = c;
+	}
+	static void bar(){
+		
+	}
+};
 
 int func1(int dummy)
 {
@@ -16,6 +29,14 @@ int main()
 {
 	frma::Perf& perf = frma::Perf::getInstance();
 	for (int i = 0; i < 10; ++i)
-		perf.profile("func1", &func1, 1, 2, 3, 4, 5);
-	perf.profile("func2", &func2);
+		perf.profile("func1", func1, 1);
+	perf.profile("func2", func2);
+	Test t;
+	perf.profile("foo", &Test::foo, &t, 5);
+	perf.profile("bar", &Test::bar);
+	if (t.a != 5)
+		std::cout<<"Test failed\n";
+	else
+		std::cout<<"Test succeeded\n";
+	return 0;
 }
