9105908d0fc90211a949988eec65eefa4f32a5ef
Aleksandar Radovic <aleksandar.radovic@syrmia.com>
2022-10-11 20:35:28 +0200

    removed macro and all the platform specific problems it caused in favor of templates

--- include/perf_test_api.h
+++ include/perf_test_api.h
@@ -3,27 +3,56 @@
 #include <fstream>
 #include <mutex>
 #include <string>
+#include <type_traits>
 #include <vector>
-
-#define profile(funcName, funcPointer, ...)                                    \
-	measureFunc(                                                               \
-		funcName,                                                              \
-		(void (*)(void))                                                       \
-			funcPointer /* dummy parameter i.e. trailing comma handler, this   \
-						   works due to the way C++ does name lookup,          \
-						   aggressively matching names in the current scope */ \
-		,                                                                      \
-		0)
 
 namespace frma {
 
 class Perf {
 public:
 	static Perf& getInstance();
-	template <typename... Args>
-	unsigned long long measureFunc(std::string funcName, void (*func)(void),
-		Args... args);
+
 	void deleteReport();
+
+	template <typename T>
+	using enableIfMethod = typename std::enable_if<std::is_member_function_pointer<T>::value, bool>::type;
+
+	template <typename T>
+	using enableIfNotMethod = typename std::enable_if<!std::is_member_function_pointer<T>::value, bool>::type;
+
+	template <typename Function, enableIfNotMethod<Function> = true, typename... Args>
+	unsigned long long profile(std::string funcName, Function&& func, Args... args)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		func(args...);
+		auto end = std::chrono::high_resolution_clock::now();
+
+		unsigned long long runTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+										 .count();
+		m.lock();
+		measurements.emplace_back(
+			std::pair<std::string, unsigned long long>(funcName, runTime));
+		m.unlock();
+		return runTime;
+	}
+
+	template <typename Function, enableIfMethod<Function> = true, typename Class, typename... Args>
+	unsigned long long profile(std::string funcName,
+		Function&& func, Class objPointer, Args... args)
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+		(objPointer->*func)(args...);
+		auto end = std::chrono::high_resolution_clock::now();
+
+		unsigned long long runTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+										 .count();
+		m.lock();
+		measurements.emplace_back(
+			std::pair<std::string, unsigned long long>(funcName, runTime));
+		m.unlock();
+		return runTime;
+	}
+
 	~Perf();
 	Perf(Perf&) = delete;
 	Perf(Perf&&) = delete;
@@ -39,23 +68,4 @@ private:
 	std::mutex m;
 };
 
-}
-
-template <typename... Args>
-unsigned long long frma::Perf::measureFunc(std::string funcName,
-	void (*func)(void), Args... args)
-{
-	void (*trueFunc)(Args...) = (void (*)(Args...))func;
-
-	auto start = std::chrono::high_resolution_clock::now();
-	trueFunc(args...);
-	auto end = std::chrono::high_resolution_clock::now();
-
-	unsigned long long runTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
-									 .count();
-	m.lock();
-	measurements.emplace_back(
-		std::pair<std::string, unsigned long long>(funcName, runTime));
-	m.unlock();
-	return runTime;
 }
